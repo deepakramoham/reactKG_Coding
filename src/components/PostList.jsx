27f861bc5ac44a1a1f589ef801466a10a1f594e8
Deepak Janardhanan <deepakramoham@gmail.com>
2024-04-29 20:40:21 +0530

    11 analysing the asynchronouse behaviour of useEffect

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,21 +1,33 @@
 import Post from "./Post";
 import { useContext, useEffect, useState } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
+import Loading from "./Loading";
 const PostList = () => {
+  const [fetching, setFetching] = useState(false);
   const { postList, addInitialPosts } = useContext(PostListData);
+  console.log("before useEffect");
   useEffect(() => {
+    setFetching(true);
+    console.log("inside useEffect, before fetchData");
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((obj) => {
         addInitialPosts(obj.posts);
+        setFetching(false);
+        console.log("inside useEffect, inside fetch, fetch returned");
       });
-  },[]);
-
+    console.log(" inside useEffect, after fetchData");
+  }, []);
+  console.log("after useEffect");
   return (
     <>
-      {postList.length !== 0 &&
+      {fetching === true && <Loading />}
+      {fetching === false &&
+        postList.length !== 0 &&
         postList.map((post) => <Post key={post.id} post={post} />)}
-      {postList.length === 0 && <center>No Posts to display...</center>}
+      {fetching === false && postList.length === 0 && (
+        <center>No Posts to display...</center>
+      )}
     </>
   );
 };
